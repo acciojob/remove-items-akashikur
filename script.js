edf61840633cb97485794260c4083efd076fd716
Akash M <108210779+akashikur@users.noreply.github.com>
2023-06-27 22:29:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function removeFunction(){
+	const select = document.getElementById("colorSelect");
+      function removeFunction() {
+        const options = document.getElementsByTagName("option");
+        for (let i = 0; i < options.length; i++) {
+          if (options[i].innerText === select.value) {
+            options[i].remove();
+          }
+        }
+      }
+} 
